0071112673b6484547c74d65e91bf26ba0a77969
Murad <mur1d1xmedov@gmail.com>
2024-01-15 01:45:56 +0400

    simp header

--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -1,11 +1,22 @@
+import logoDark from "../../../assets/images/logo-dark.png";
 import styles from "./assets/css/styles.module.css";
 
 const Header = () => {
   return (
     <>
-      <div className="wrapper">
+      <div className={`wrapper ${styles.wrapper}`}>
         <div className="container">
-          <div className={styles.container}>test</div>
+          <div className={styles.container}>
+            <div className="col col-2 text-start">
+              <div className={styles.logo}>
+                <img src={logoDark} alt="logo-dark" />
+              </div>
+            </div>
+            <div className="col col-8 text-center">nav list</div>
+            <div className="col col-2 text-end">
+              <button className={styles.btnJoin}>join now</button>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,3 +2,8 @@ declare module "*.module.css" {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpeg";
+declare module "*.jpg";
