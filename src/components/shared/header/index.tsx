483476bbfd69918535fceff29976af2d0e209516
Murad <mur1d1xmedov@gmail.com>
2024-01-15 18:53:04 +0400

    header without navigation

--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -7,14 +7,16 @@ const Header = () => {
       <div className={`wrapper ${styles.wrapper}`}>
         <div className="container">
           <div className={styles.container}>
-            <div className="col col-2 text-start">
+            <div className="col col-5 col-lg-3 text-start">
               <div className={styles.logo}>
                 <img src={logoDark} alt="logo-dark" />
               </div>
             </div>
-            <div className="col col-8 text-center">nav list</div>
-            <div className="col col-2 text-end">
+            <div className="col col-lg-2 d-none d-md-block text-end order-lg-3 ">
               <button className={styles.btnJoin}>join now</button>
+            </div>
+            <div className="col col-2 col-md-1 col-lg-7 text-end text-lg-center">
+              nav
             </div>
           </div>
         </div>
