43f133dc1d1cc7a213bfca8f32807adce2d6786f
DESKTOP-KTIOEGP\Murad Ahmadov <murad.ahmadov@vabiss.com>
2024-04-30 02:48:11 +0400

    - loading to home page not in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import { useState, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './pages/router';
-import LoadingSpinner from './components/shared/loadingSpinner';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  return <BrowserRouter>{loading ? <LoadingSpinner /> : <Router />}</BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/main/home/index.tsx
+++ src/pages/main/home/index.tsx
@@ -1,11 +1,19 @@
-import HomeContainer from "@/containers/home";
+import { useState, useEffect } from 'react';
+import HomeContainer from '@/containers/home';
+import LoadingSpinner from '@/components/shared/loadingSpinner';
 
 const Home = () => {
-  return (
-    <>
-      <HomeContainer />
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <>{loading ? <LoadingSpinner /> : <HomeContainer />}</>;
 };
 
 export default Home;
