d32f8088f8069e8405d75197be1a067b45ca163c
DESKTOP-KTIOEGP\Murad Ahmadov <murad.ahmadov@vabiss.com>
2024-04-30 04:51:10 +0400

    - apply lazy loading to all routes

--- src/pages/main/home/index.tsx
+++ src/pages/main/home/index.tsx
@@ -8,7 +8,7 @@ const Home = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 1500);
 
     return () => clearTimeout(timer);
   }, []);
